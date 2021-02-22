b637f155eeb1d5815f039d7a16dc29dda0e74c95
affisjunianto <69682437+affisjunianto@users.noreply.github.com>
2021-02-22 20:03:50 +0700

    Add files via upload

--- language/ind.js
+++ language/ind.js
@@ -1,3 +1,5 @@
+const a = '```'
+
 exports.wait = () => {
 	return`*「 WAIT 」 SEDANG PROSES*`
 }
@@ -131,113 +133,122 @@ exports.menu = (pushname, prefix, getLevelingLevel, getLevelingXp, sender, reqXp
  *${prefix}donasi*
  *${prefix}owner*
 
-┏━━⊱ *MAKER MENU* ⊰━━┓
-┣⊱ *${prefix}sticker* 
-┣⊱ *${prefix}quotemaker* <text|author>
-┣⊱ *${prefix}qrcode* <text>
-┣⊱ *${prefix}ttp*
-┣⊱ *${prefix}stickerhide* <text>
-┣⊱ *${prefix}emoji*
-┣━━⊱  *FUN MENU*  ⊰━━━┫
-┣⊱ *${prefix}lirik* <text>
-┣⊱ *${prefix}artinama*
-┣⊱ *${prefix}chord* <text>
-┣⊱ *${prefix}bisakah* <teks>
-┣⊱ *${prefix}kapankah* <teks>
-┣⊱ *${prefix}apakah* <teks>
-┣⊱ *${prefix}rate* <teks>
-┣⊱ *${prefix}slap*
-┣⊱ *${prefix}tampar*
-┣⊱ *${prefix}moddroid* <teks> *[VIP]*
-┣⊱ *${prefix}happymod* <teks> *[VIP]*
-┣━━⊱ *MUTUAL* ⊰━━━━━┫
-┣⊱ *${prefix}mutual*
-┣⊱ *${prefix}next*
-┣━━⊱ *MEDIA MENU* ⊰━━┫
-┣⊱ *${prefix}brainly* *[VIP]*
-┣⊱ *${prefix}pinterest*
-┣⊱ *${prefix}resepmasakan* <teks>
-┣⊱ *${prefix}igstalk* <username>
-┣⊱ *${prefix}bitly* <link>
-┣⊱ *${prefix}tiktok* <username> *[VIP]*
-┣⊱ *${prefix}tiktokstalk* <username> *[VIP]*
-┣⊱ *${prefix}ssweb* <link>
-┣⊱ *${prefix}kbbi* <text>
-┣━━⊱ *YT & SONG* ⊰━━━┫
-┣⊱ *${prefix}ytmp3* <link> *[VIP]*
-┣⊱ *${prefix}ytmp4* <link> *[VIP]*
-┣⊱ *${prefix}joox* <judul> *[VIP]*
-┣━━⊱  *NSFW MENU*  ⊰━┫
-┣⊱ *${prefix}anjing*
-┣⊱ *${prefix}blowjob*
-┣⊱ *${prefix}nekonime*
-┣⊱ *${prefix}pokemon*
-┣⊱ *${prefix}husbu*
-┣⊱ *${prefix}nangis*
-┣⊱ *${prefix}cium*
-┣⊱ *${prefix}peluk*
-┣⊱ *${prefix}ranime*
-┣━━⊱ *LIMIT & UANG* ⊰━┫
-┣⊱ *${prefix}limit* 
-┣⊱ *${prefix}buylimit* <jumblah>
-┣⊱ *${prefix}transfer* <tag |jumblah>
-┣⊱ *${prefix}dompet*
-┣⊱ *${prefix}giftlimit* <tag jumblah>
-┣⊱ *${prefix}leaderboard* <jumblah>
-┣━━⊱ *GROUP MENU* ⊰━┫
-┣⊱ *${prefix}delete* <tag message>
-┣⊱ *${prefix}hidetag*
-┣⊱ *${prefix}blocklist*
-┣⊱ *${prefix}grouplist*
-┣⊱ *${prefix}level*
-┣⊱ *${prefix}linkgc*
-┣⊱ *${prefix}tagall*
-┣⊱ *${prefix}setpp*
-┣⊱ *${prefix}add* <tag>
-┣⊱ *${prefix}kick* <tag>
-┣⊱ *${prefix}setname* <teks>
-┣⊱ *${prefix}setdesc* <teks>
-┣⊱ *${prefix}demote* <tag>
-┣⊱ *${prefix}promote* <tag>
-┣⊱ *${prefix}listadmin*
-┣⊱ *${prefix}group* [buka/tutup]
-┣⊱ *${prefix}leveling* [enable/disable]
-┣⊱ *${prefix}nsfw* [1/0]
-┣⊱ *${prefix}simih* [1/0]
-┣⊱ *${prefix}welcome* [1/0]
-┣⊱ *${prefix}antilink* [1/0]
-┣⊱ *${prefix}nobadword* [enable/disable]
-┣━━⊱ *OWNER MENU* ⊰━┫
-┣⊱ *${prefix}bc* <teks>
-┣⊱ *${prefix}addbadword* <text>
-┣⊱ *${prefix}delbadword* <text>
-┣⊱ *${prefix}bcgc* <teks>
-┣⊱ *${prefix}kickall* <rawan ban>
-┣⊱ *${prefix}setreply* <teks>
-┣⊱ *${prefix}setprefix* <symbol>
-┣⊱ *${prefix}clearall*
-┣⊱ *${prefix}block* <tag>
-┣⊱ *${prefix}unblock* <tag>
-┣⊱ *${prefix}leave*
-┣⊱ *${prefix}event* [1/0]
-┣⊱ *${prefix}clone* <tag>
-┣⊱ *${prefix}setppbot*
-┣━━⊱ *THANGKS TO* ⊰━━┫
-┃
-┣⊱ *AFFIS JUNIANTO* <dev>
-┣⊱ *FADHIL GRAPHY* <partner>
-┣⊱ *MHANKBARBARS* <sc ori>
-┣⊱ *MYBOT TEAM* <team>
-┃
-┣⊱ NOTE : TQTO DI HAPUS
-┃ GW GAK UP LAGI OKE
-┃
-┗━━⊱  ⸨ *X-BOT* ⸩  ⊰━━━━┛
+${a}❏MAKER MENU${a}
+ ${a}╿•${prefix}sticker${a}
+ ${a}╿•${prefix}quotemaker${a}
+ ${a}╿•${prefix}qrcode${a}
+ ${a}╰•${prefix}ttp${a}
+${a}❏FUN MENU${a}
+ ${a}╿•${prefix}lirik${a}
+ ${a}╿•${prefix}artinama${a}
+ ${a}╿•${prefix}chord${a}
+ ${a}╿•${prefix}bisakah${a}
+ ${a}╿•${prefix}kapankah${a}
+ ${a}╿•${prefix}apakah${a}
+ ${a}╿•${prefix}rate${a}
+ ${a}╿•${prefix}slap${a}
+ ${a}╿•${prefix}tampar${a}
+ ${a}╿•${prefix}moddroid${a} *[VIP]*
+ ${a}╰•${prefix}happymod${a} *[VIP]*
+${a}❏MUTUAL${a}
+ ${a}╿•${prefix}mutual${a}
+ ${a}╰•${prefix}next${a}
+${a}❏MEDIA MENU${a}
+ ${a}╿•${prefix}brainly${a} *[VIP]*
+ ${a}╿•${prefix}pinterest${a}
+ ${a}╿•${prefix}resepmasakan${a}
+ ${a}╿•${prefix}igstalk${a}
+ ${a}╿•${prefix}bitly${a}
+ ${a}╿•${prefix}tiktokstalk${a} *[VIP]*
+ ${a}╿•${prefix}ssweb${a}
+ ${a}╰•${prefix}kbbi${a}
+${a}❏SONG${a}
+ ${a}╰•${prefix}joox${a} *[VIP]*
+${a}❏NSFW${a}
+ ${a}╿•${prefix}anjing${a}
+ ${a}╿•${prefix}blowjob${a}
+ ${a}╿•${prefix}nekonime${a}
+ ${a}╿•${prefix}pokemon${a}
+ ${a}╿•${prefix}husbu${a}
+ ${a}╿•${prefix}nangis${a}
+ ${a}╿•${prefix}cium${a}
+ ${a}╿•${prefix}peluk${a}
+ ${a}╰•${prefix}ranime${a}
+${a}❏LIMIT & UANG${a}
+ ${a}╿•${prefix}limit${a}
+ ${a}╿•${prefix}buylimit${a}
+ ${a}╿•${prefix}transfer${a}
+ ${a}╿•${prefix}dompet${a}
+ ${a}╿•${prefix}giftlimit${a}
+ ${a}╰•${prefix}leaderboard${a}
+${a}❏GROUP MENU${a}
+ ${a}╿•${prefix}delete${a}
+ ${a}╿•${prefix}hidetag${a}
+ ${a}╿•${prefix}blocklist${a}
+ ${a}╿•${prefix}grouplist${a}
+ ${a}╿•${prefix}level${a}
+ ${a}╿•${prefix}linkgc${a}
+ ${a}╿•${prefix}tagall${a}
+ ${a}╿•${prefix}setpp${a}
+ ${a}╿•${prefix}add${a}
+ ${a}╿•${prefix}kick${a}
+ ${a}╿•${prefix}setname${a}
+ ${a}╿•${prefix}setdesc${a}
+ ${a}╿•${prefix}demote${a}
+ ${a}╿•${prefix}promote${a}
+ ${a}╿•${prefix}listadmin${a}
+ ${a}╿•${prefix}group [buka/tutup]${a}
+ ${a}╿•${prefix}leveling [enable/disable]${a}
+ ${a}╿•${prefix}nsfw [1/0]${a}
+ ${a}╿•${prefix}simih [1/0]${a}
+ ${a}╿•${prefix}welcome [1/0]${a}
+ ${a}╿•${prefix}antilink [1/0]${a}
+ ${a}╰•${prefix}nobadword [enable/disable]${a}
+${a}❏CLOUD STORAGE${a}
+ ${a}╿•${prefix}addsticker${a}
+ ${a}╿•${prefix}getsticker${a}
+ ${a}╿•${prefix}stickerlist${a}
+ ${a}╿•${prefix}addvideo${a}
+ ${a}╿•${prefix}getvideo${a}
+ ${a}╿•${prefix}videolist${a}
+ ${a}╿•${prefix}getimage${a}
+ ${a}╿•${prefix}addImage${a}
+ ${a}╿•${prefix}imagelist${a}
+ ${a}╿•${prefix}addaudio${a}
+ ${a}╿•${prefix}getaudio${a}
+ ${a}╰•${prefix}audiolist${a}
+${a}❏OWNER MENU${a}
+ ${a}╿•${prefix}bc${a}
+ ${a}╿•${prefix}addbadword${a}
+ ${a}╿•${prefix}delbadword${a}
+ ${a}╿•${prefix}bcgc${a}
+ ${a}╿•${prefix}kickall${a}
+ ${a}╿•${prefix}setreply${a}
+ ${a}╿•${prefix}setprefix${a}
+ ${a}╿•${prefix}clearall${a}
+ ${a}╿•${prefix}block${a}
+ ${a}╿•${prefix}unblock${a}
+ ${a}╿•${prefix}leave${a}
+ ${a}╿•${prefix}event [1/0]${a}
+ ${a}╿•${prefix}clone${a}
+ ${a}╰•${prefix}setppbot${a}
+ 
+${a}❏THANGKS TO${a}
+ ${a}╿${a}
+ ${a}╿ • AFFIS JUNIANTO <dev>${a}
+ ${a}╿ • FADHIL GRAPHY <partner>${a}
+ ${a}╿ • MHANKBARBARS <sc ori>${a}
+ ${a}╿ • MYBOT TEAM <team>${a}
+ ${a}╿${a}
+ ${a}╿ • NOTE : jangan ubah donasi${a}
+ ${a}╿${a}
+ ${a}╰━━━⊱  ⸨ X-BOT ⸩  ⊰━━━━${a}
 `
 }
 
 exports.levelup = (pushname, sender, getLevelingXp,  getLevel, getLevelingLevel, role) => {
 	return`
+	
 *「 SELAMAT 」*
 ┏⊱ *Nama* : ${pushname}
 ┣⊱ *Nomer* : wa.me/${sender.split("@")[0]}
