30d742f4e27fb0a9595852f411c8dc5f8aec3333
affisjunianto <69682437+affisjunianto@users.noreply.github.com>
2021-03-01 00:09:50 +0700

    Add files via upload

--- language/ind.js
+++ language/ind.js
@@ -116,133 +116,161 @@ exports.reglevelahf = (command, pushname, getLevelingLevel, sender, ahf) => {
 	return`*Maaf ${pushname} level mu belum mencukupi*\n\n*┏⊱level mu : ${getLevelingLevel(sender)}*\n*┣⊱jenis command : ${command}*\n*┗⊱syarat level : ${ahf}*\n\n_NOTE : CHAT/SELALU ON UNTUK MENDAPATKAN XP_`
 }
 
-exports.menu = (pushname, prefix, getLevelingLevel, getLevelingXp, sender, reqXp, _registered, uangku, role, premi) => { 
+exports.menu = (pushname, prefix, getLevelingLevel, getLevelingXp, sender, reqXp, _registered, uangku, role, premi, client , process) => { 
 	return `
-┏━━━⊱  *ABOUT USER*  ⊰━━┓
-┣⊱ *Nama* : ${pushname}
-┣⊱ *Premium* : ${premi}
-┣⊱ *Nomer* : wa.me/${sender.split("@")[0]}
-┣⊱ *Uang mu* : Rp${uangku}
-┣⊱ *XP* : ${getLevelingXp(sender)}/${reqXp} 
-┣⊱ *Level* : ${getLevelingLevel(sender)}
-┣⊱ *Role* : ${role}
-┣⊱ *User register* : ${_registered.length}
-┗━━━⊱  ⸨ *AFFIS* ⸩  ⊰━━━━┛
+${a}❏ ABOUT USER${a}
+ ${a}│ Nama : ${pushname}${a}
+ ${a}│ Premium :${a} ${premi}
+ ${a}│ Nomer : wa.me/${sender.split("@")[0]}${a}
+ ${a}│ Uang mu : Rp${uangku}${a}
+ ${a}│ XP : ${getLevelingXp(sender)}/${reqXp} ${a}
+ ${a}│ Level : ${getLevelingLevel(sender)}${a}
+ ${a}│ Role : ${role}${a}
+ ${a}╰ User register : ${_registered.length}${a}
 
  *${prefix}info*
  *${prefix}donasi*
  *${prefix}owner*
 
 ${a}❏MAKER MENU${a}
- ${a}╿•${prefix}sticker${a}
- ${a}╿•${prefix}quotemaker${a}
- ${a}╿•${prefix}qrcode${a}
+ ${a}│•${prefix}sticker${a}
+ ${a}│•${prefix}quotemaker${a}
+ ${a}│•${prefix}qrcode${a}
+ ${a}│•${prefix}nulis${a}
+ ${a}│•${prefix}tahta${a}
  ${a}╰•${prefix}ttp${a}
+ 
 ${a}❏FUN MENU${a}
- ${a}╿•${prefix}lirik${a}
- ${a}╿•${prefix}artinama${a}
- ${a}╿•${prefix}chord${a}
- ${a}╿•${prefix}bisakah${a}
- ${a}╿•${prefix}kapankah${a}
- ${a}╿•${prefix}apakah${a}
- ${a}╿•${prefix}rate${a}
- ${a}╿•${prefix}slap${a}
- ${a}╿•${prefix}tampar${a}
- ${a}╿•${prefix}moddroid${a} *[VIP]*
+ ${a}│•${prefix}lirik${a}
+ ${a}│•${prefix}artinama${a}
+ ${a}│•${prefix}chord${a}
+ ${a}│•${prefix}bisakah${a}
+ ${a}│•${prefix}kapankah${a}
+ ${a}│•${prefix}apakah${a}
+ ${a}│•${prefix}rate${a}
+ ${a}│•${prefix}tebakgambar${a}
+ ${a}│•${prefix}meme${a}
+ ${a}│•${prefix}textlight${a}
+ ${a}│•${prefix}glitchtext${a}
+ ${a}│•${prefix}slap${a}
+ ${a}│•${prefix}tampar${a}
+ ${a}│•${prefix}moddroid${a} *[VIP]*
  ${a}╰•${prefix}happymod${a} *[VIP]*
+ 
 ${a}❏MUTUAL${a}
- ${a}╿•${prefix}mutual${a}
+ ${a}│•${prefix}mutual${a}
  ${a}╰•${prefix}next${a}
+ 
 ${a}❏MEDIA MENU${a}
- ${a}╿•${prefix}brainly${a} *[VIP]*
- ${a}╿•${prefix}pinterest${a}
- ${a}╿•${prefix}resepmasakan${a}
- ${a}╿•${prefix}igstalk${a}
- ${a}╿•${prefix}bitly${a}
- ${a}╿•${prefix}tiktokstalk${a} *[VIP]*
- ${a}╿•${prefix}ssweb${a}
+ ${a}│•${prefix}brainly${a} *[VIP]*
+ ${a}│•${prefix}pinterest${a}
+ ${a}│•${prefix}resepmasakan${a}
+ ${a}│•${prefix}igstalk${a}
+ ${a}│•${prefix}bitly${a}
+ ${a}│•${prefix}tiktokstalk${a} *[VIP]*
+ ${a}│•${prefix}ssweb${a}
  ${a}╰•${prefix}kbbi${a}
+ 
 ${a}❏SONG${a}
  ${a}╰•${prefix}joox${a} *[VIP]*
+ 
 ${a}❏NSFW${a}
- ${a}╿•${prefix}anjing${a}
- ${a}╿•${prefix}blowjob${a}
- ${a}╿•${prefix}nekonime${a}
- ${a}╿•${prefix}pokemon${a}
- ${a}╿•${prefix}husbu${a}
- ${a}╿•${prefix}nangis${a}
- ${a}╿•${prefix}cium${a}
- ${a}╿•${prefix}peluk${a}
+ ${a}│•${prefix}anjing${a}
+ ${a}│•${prefix}blowjob${a}
+ ${a}│•${prefix}nekonime${a}
+ ${a}│•${prefix}pokemon${a}
+ ${a}│•${prefix}husbu${a}
+ ${a}│•${prefix}nangis${a}
+ ${a}│•${prefix}cium${a}
+ ${a}│•${prefix}peluk${a}
  ${a}╰•${prefix}ranime${a}
+ 
 ${a}❏LIMIT & UANG${a}
- ${a}╿•${prefix}limit${a}
- ${a}╿•${prefix}buylimit${a}
- ${a}╿•${prefix}transfer${a}
- ${a}╿•${prefix}dompet${a}
- ${a}╿•${prefix}giftlimit${a}
+ ${a}│•${prefix}limit${a}
+ ${a}│•${prefix}buylimit${a}
+ ${a}│•${prefix}transfer${a}
+ ${a}│•${prefix}dompet${a}
+ ${a}│•${prefix}giftlimit${a}
  ${a}╰•${prefix}leaderboard${a}
+ 
 ${a}❏GROUP MENU${a}
- ${a}╿•${prefix}delete${a}
- ${a}╿•${prefix}hidetag${a}
- ${a}╿•${prefix}blocklist${a}
- ${a}╿•${prefix}grouplist${a}
- ${a}╿•${prefix}level${a}
- ${a}╿•${prefix}linkgc${a}
- ${a}╿•${prefix}tagall${a}
- ${a}╿•${prefix}setpp${a}
- ${a}╿•${prefix}add${a}
- ${a}╿•${prefix}kick${a}
- ${a}╿•${prefix}setname${a}
- ${a}╿•${prefix}setdesc${a}
- ${a}╿•${prefix}demote${a}
- ${a}╿•${prefix}promote${a}
- ${a}╿•${prefix}listadmin${a}
- ${a}╿•${prefix}group [buka/tutup]${a}
- ${a}╿•${prefix}leveling [enable/disable]${a}
- ${a}╿•${prefix}nsfw [1/0]${a}
- ${a}╿•${prefix}simih [1/0]${a}
- ${a}╿•${prefix}welcome [1/0]${a}
- ${a}╿•${prefix}antilink [1/0]${a}
+ ${a}│•${prefix}delete${a}
+ ${a}│•${prefix}hidetag${a}
+ ${a}│•${prefix}blocklist${a}
+ ${a}│•${prefix}grouplist${a}
+ ${a}│•${prefix}level${a}
+ ${a}│•${prefix}linkgc${a}
+ ${a}│•${prefix}tagall${a}
+ ${a}│•${prefix}setpp${a}
+ ${a}│•${prefix}add${a}
+ ${a}│•${prefix}kick${a}
+ ${a}│•${prefix}setname${a}
+ ${a}│•${prefix}setdesc${a}
+ ${a}│•${prefix}demote${a}
+ ${a}│•${prefix}promote${a}
+ ${a}│•${prefix}listadmin${a}
+ ${a}│•${prefix}group [buka/tutup]${a}
+ ${a}│•${prefix}leveling [enable/disable]${a}
+ ${a}│•${prefix}nsfw [1/0]${a}
+ ${a}│•${prefix}simih [1/0]${a}
+ ${a}│•${prefix}welcome [1/0]${a}
+ ${a}│•${prefix}antilink [1/0]${a}
  ${a}╰•${prefix}nobadword [enable/disable]${a}
+ 
+${a}❏TOOLS${a}
+ ${a}│•${prefix}bass${a}
+ ${a}│•${prefix}tomp3${a}
+ ${a}│•${prefix}slowmo${a}
+ ${a}│•${prefix}gemok${a}
+ ${a}╰•${prefix}tupai${a}
+ 
 ${a}❏CLOUD STORAGE${a}
- ${a}╿•${prefix}addsticker${a}
- ${a}╿•${prefix}getsticker${a}
- ${a}╿•${prefix}stickerlist${a}
- ${a}╿•${prefix}addvideo${a}
- ${a}╿•${prefix}getvideo${a}
- ${a}╿•${prefix}videolist${a}
- ${a}╿•${prefix}getimage${a}
- ${a}╿•${prefix}addImage${a}
- ${a}╿•${prefix}imagelist${a}
- ${a}╿•${prefix}addaudio${a}
- ${a}╿•${prefix}getaudio${a}
+ ${a}│•${prefix}addsticker${a}
+ ${a}│•${prefix}getsticker${a}
+ ${a}│•${prefix}stickerlist${a}
+ ${a}│•${prefix}addvideo${a}
+ ${a}│•${prefix}getvideo${a}
+ ${a}│•${prefix}videolist${a}
+ ${a}│•${prefix}getimage${a}
+ ${a}│•${prefix}addImage${a}
+ ${a}│•${prefix}imagelist${a}
+ ${a}│•${prefix}addaudio${a}
+ ${a}│•${prefix}getaudio${a}
  ${a}╰•${prefix}audiolist${a}
+ 
 ${a}❏OWNER MENU${a}
- ${a}╿•${prefix}bc${a}
- ${a}╿•${prefix}addbadword${a}
- ${a}╿•${prefix}delbadword${a}
- ${a}╿•${prefix}bcgc${a}
- ${a}╿•${prefix}kickall${a}
- ${a}╿•${prefix}setreply${a}
- ${a}╿•${prefix}setprefix${a}
- ${a}╿•${prefix}clearall${a}
- ${a}╿•${prefix}block${a}
- ${a}╿•${prefix}unblock${a}
- ${a}╿•${prefix}leave${a}
- ${a}╿•${prefix}event [1/0]${a}
- ${a}╿•${prefix}clone${a}
+ ${a}│•${prefix}bc${a}
+ ${a}│•${prefix}addbadword${a}
+ ${a}│•${prefix}delbadword${a}
+ ${a}│•${prefix}bcgc${a}
+ ${a}│•${prefix}kickall${a}
+ ${a}│•${prefix}setreply${a}
+ ${a}│•${prefix}setprefix${a}
+ ${a}│•${prefix}clearall${a}
+ ${a}│•${prefix}block${a}
+ ${a}│•${prefix}unblock${a}
+ ${a}│•${prefix}leave${a}
+ ${a}│•${prefix}event [1/0]${a}
+ ${a}│•${prefix}clone${a}
  ${a}╰•${prefix}setppbot${a}
  
+${a}❏ABOUT BOT${a}
+ ${a}│ • Name : ${client.user.name}${a}
+ ${a}│ • browser : ${client.browserDescription[1]}${a}
+ ${a}│ • server : ${client.browserDescription[0]}${a}
+ ${a}│ • version : ${client.browserDescription[2]}${a}
+ ${a}│ • speed : ${process.uptime()}${a}
+ ${a}│ • handphone : ${client.user.phone.device_manufacturer}${a}
+ ${a}╰ • versi wa : ${client.user.phone.wa_version}${a}
+ 
 ${a}❏THANGKS TO${a}
- ${a}╿${a}
- ${a}╿ • AFFIS JUNIANTO <dev>${a}
- ${a}╿ • FADHIL GRAPHY <partner>${a}
- ${a}╿ • MHANKBARBARS <sc ori>${a}
- ${a}╿ • MYBOT TEAM <team>${a}
- ${a}╿${a}
- ${a}╿ • NOTE : jangan ubah donasi${a}
- ${a}╿${a}
- ${a}╰━━━⊱  ⸨ X-BOT ⸩  ⊰━━━━${a}
+ ${a}│${a}
+ ${a}│ • AFFIS JUNIANTO <dev>${a}
+ ${a}│ • FADHIL GRAPHY <partner>${a}
+ ${a}│ • MHANKBARBARS <sc ori>${a}
+ ${a}│ • MYBOT TEAM <team>${a}
+ ${a}│${a}
+ ${a}╰ • NOTE : jangan ngemis${a}
 `
 }
 
